448846148935809644b21585eb1235291534d18a
Clayton Haddock <clayton.haddock@gmail.com>
2019-03-17 13:44:44 -0700

    removed mongo connection uri

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const serverConfig = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://appointmentsuser:Kronwall55@ds259154.mlab.com:59154/appointments',
-  port: process.env.PORT || 3001
+  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/appointments'
 };
 
 module.exports = serverConfig;

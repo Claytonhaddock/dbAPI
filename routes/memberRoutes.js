81506bd5d3e089d1939b79a2f8ad6b47bde29b70
Clayton Haddock <clayton.haddock@gmail.com>
2019-03-19 14:18:37 -0700

    updated member route

--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -51,7 +51,7 @@ router.delete('/:id', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  console.log(req.body);
+  console.log('body: ', req);
   PersonModel.findByIdAndUpdate(
     req.params.id, 
     req.body, 

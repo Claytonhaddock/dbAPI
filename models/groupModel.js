53cd98e331a5d3caeda6ff54cac91c9d921737c9
Clayton Haddock <clayton.haddock@gmail.com>
2019-03-25 16:00:37 -0700

    fixed errors

--- models/groupModel.js
+++ models/groupModel.js
@@ -5,15 +5,15 @@ const GroupSchema = new Schema({
   creator: {
       type: String,
       required: true
-    },
+  },
   test: {
   	type: String,
   	required: true
-  }
+  },
   name: {
       type: String,
       required: true
-    }
+  },
   members: [{ type: Schema.Types.ObjectId, ref: 'Person' }]
 });
 const GroupModel = mongoose.model('Group', GroupSchema);

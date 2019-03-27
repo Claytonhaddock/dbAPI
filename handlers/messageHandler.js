d6da5a1c54b1a2f99222272c6b510aab185ae71d
Clayton Haddock <clayton.haddock@gmail.com>
2019-03-26 17:55:05 -0700

    testing more assertions

--- handlers/messageHandler.js
+++ handlers/messageHandler.js
@@ -58,6 +58,8 @@ function unpaidSort(arr){
 function timeUntilDue(group){
 	const now = moment();
 	const due = moment(group.duedate);
+	console.log(now.format('YYYY-MM-DD'))
+	console.log(due.format('YYYY-MM-DD'))
 	console.log('time diff: ', now.diff(due, 'days'));
 	return now.diff(due, 'days');
 }
